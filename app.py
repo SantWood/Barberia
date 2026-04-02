bf20cc5c062a12408401e7ce2eab9373957ac512
SantWoodz <eversonmadera7@gmail.com>
2026-04-01 20:49:12 -0500

    configuracion para deploy en Render

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
+import os
 from flask import Flask, render_template
 from flask_cors import CORS
 from config import Config
 from utils.db import close_db
-from routes import clientes_bp, barberos_bp, citas_bp, reportes_bp
+from routes import clientes_bp, barberos_bp, citas_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
 CORS(app)
 
 app.register_blueprint(clientes_bp)
-app.register_blueprint(reportes_bp)
 app.register_blueprint(barberos_bp)
 app.register_blueprint(citas_bp)
 app.teardown_appcontext(close_db)
@@ -30,10 +30,6 @@ def vista_clientes():
 def vista_barberos():
     return render_template("barberos.html")
 
-@app.route("/reportes")
-def vista_reportes():
-    return render_template("reportes.html")
-
 if __name__ == "__main__":
-    print("BarberApp corriendo en http://localhost:5000")
-    app.run(debug=app.config["DEBUG"], port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
