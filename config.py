fa45c5f95a7c94b1d979f073e103d00c008f73c8
SantWoodz <eversonmadera7@gmail.com>
2026-04-03 18:03:13 -0500

    sidebar con login logout y roles en todas las paginas

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
+    SECRET_KEY = os.getenv("SECRET_KEY", "barberia_clave_secreta_2024")
     MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/barberia_db")
-    DEBUG = os.getenv("FLASK_DEBUG", "True") == "True"
+    DEBUG = os.getenv("FLASK_DEBUG", "False") == "True"
+    SESSION_COOKIE_SAMESITE = "Lax"
+    SESSION_COOKIE_SECURE = False
